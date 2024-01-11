7136340990d9a57c643f78dccd301e107a5b95f1
amirreza <yar.amirreza@gmail.com>
2024-01-11 21:46:59 +0330

    uuid shorten

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
+import base64
 
 class CustomUser(AbstractUser):
     UUID_FIELD = 'uuid'
-    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    
+    uuid = models.CharField(max_length=22, unique=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.uuid:
+            self.uuid = base64.urlsafe_b64encode(uuid.uuid4().bytes).rstrip(b'=').decode('utf-8')[:8]
+        super().save(*args, **kwargs)
